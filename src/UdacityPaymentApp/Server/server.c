8f151bb0246799adcf96a15aa9a438edb17bc88a
Mohammed Elghamry <44609329+melgmry0101b@users.noreply.github.com>
2022-12-23 13:42:59 +0200

    feat: add global data variables

--- src/UdacityPaymentApp/Server/server.c
+++ src/UdacityPaymentApp/Server/server.c
@@ -16,6 +16,47 @@
 #include "../Application/app.h"
 #include "server.h"
 
+// ==================
+// ====== Defs ======
+// ==================
+
+#define MAX_ENTRIES 255
+
+// =====================
+// ====== Globals ======
+// =====================
+
+// NOTE: I would've implemented theses globals into an `sqlite` database,
+//  but as I am running tight on time, I would stick with the required
+//  parameters for now.
+
+// ---------------------------------------------
+// Global variable holding accounts.
+// ---------------------------------------------
+static ST_accountsDB_t accountsDB[MAX_ENTRIES] = {
+    { 2000.0, RUNNING, "8989374615436851" },
+    { 100000.0, BLOCKED, "5807007076043875" },
+    { 198000.0, BLOCKED, "5610591081018250" },
+    { 0.0, RUNNING, "378282246310005" },
+    { 500.0, RUNNING, "371449635398431" },
+    { 5200.0, RUNNING, "378734493671000" },
+    { 300.0, RUNNING, "30569309025904" },
+    { 15900.0, BLOCKED, "38520000023237" },
+    { 4000.0, BLOCKED, "6011111111111117" },
+    { 0.0, RUNNING, "6011000990139424" }
+};
+
+// ---------------------------------------------
+// Global variable holding transactions.
+// ---------------------------------------------
+static ST_transaction_t transactionsDB[MAX_ENTRIES] = { 0 };
+
+// ---------------------------------------------
+// Global variable telling how many transactions
+//  we have saved.
+// ---------------------------------------------
+static uint16_t transactionsCount = 0;
+
 // =====================
 // ====== Methods ======
 // =====================
